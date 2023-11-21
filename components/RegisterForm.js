3c1d99ae637f471be903007c33c774db16910317
MeetHansalia <meethansalia99@gmail.com>
2023-11-21 16:34:19 +0530

    change button color

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -145,19 +145,17 @@ const RegisterForm = ({onRegister}) => {
                  {...register('confirmPassword')}                
                  className={`${ errors.confirmPassword ? 'border-red-500':''} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white`}
                  required
-                 
                />
                 { errors.confirmPassword && <p className="text-red-500 text-xs mt-1">{errors.confirmPassword.message}</p>}
              </div>
              <button
                type="submit"
-               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+               className="w-full text-black bg-white hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
              >
                Create Account
              </button>
-             <div className="text-sm font-medium text-gray-500 dark:text-white">
-              
-              <Link href="/" className="text-blue-700 hover:underline">
+             <div className="text-sm font-medium text-gray-500 dark:text-white text-center">
+              <Link href="/" className="text-white-700  hover:underline">
                 Login Page
               </Link>
              </div>
