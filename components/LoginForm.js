5b68b5867c4f8f32c925f33731c4f61f57910ad6
MeetHansalia <meethansalia99@gmail.com>
2023-11-21 18:44:28 +0530

    navBar changes

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -84,13 +84,13 @@ const LoginForm = ({ onLogin, userData }) => {
             </div>
             <button
               type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              className="w-full text-black bg-white hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               Login to your account
             </button>
-            <div className="text-sm font-medium text-gray-500 dark:text-white">
+            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-white ">
               Not registered?{' '}
-              <Link href="/register" className="text-blue-700 hover:underline">
+              <Link href="/register" className="text-white-700 hover:underline">
                 Create account
               </Link>
             </div>
